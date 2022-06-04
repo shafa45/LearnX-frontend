a47b43327b17736c9155abf0f83408b31245a649
shafa45 <safaullah14@gmail.com>
2022-06-05 02:05:32 +0530

    very basic client setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const Index = () => {
+  return (
+    <>
+      <h1 className='jumbotron text-center bg-primary square'>
+        Online Education Marketplace
+      </h1>
+      <p>NextJs</p>
+    </>
+  );
+};
+
+export default Index;
---
+++ pages/login.jsx
@@ -0,0 +1,8 @@
+export default function login() {
+  return (
+    <>
+      <h1 className='jumbotron bg-primary square'>Login</h1>
+      <p>login page</p>
+    </>
+  );
+}
