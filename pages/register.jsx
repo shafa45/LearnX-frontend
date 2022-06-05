6fb47d4e60326f0de4f0a36b31521014436f5c9e
shafa45 <safaullah14@gmail.com>
2022-06-05 19:51:47 +0530

    ENV AND GIT

--- pages/register.jsx
+++ pages/register.jsx
@@ -15,11 +15,14 @@ export default function Register() {
 
     try {
       setLoading(true);
-      const response = await axios.post(`http://localhost:8000/api/register`, {
-        name,
-        email,
-        password,
-      });
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_TEST}/register`,
+        {
+          name,
+          email,
+          password,
+        }
+      );
       // console.log(response.data.message);
       setLoading(false);
       toast.success(response.data.message + ' Redirecting to login page...');
