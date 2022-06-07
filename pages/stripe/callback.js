77d348563e34a24bf481d770d1bf3c775f3e13f7
shafa45 <safaullah14@gmail.com>
2022-06-07 19:16:25 +0530

    testing..

--- pages/stripe/callback.js
+++ pages/stripe/callback.js
@@ -11,14 +11,15 @@ const StripeCallback = () => {
   useEffect(() => {
     if (user)
       axios.post('/api/get-account-status').then((res) => {
-        window.location.href = '/instructor';
+        // window.location.href = '/instructor';
+        console.log(res.data);
       });
   }, [user]);
 
   return (
     <SyncOutlined
       spin
-      className='d-flex justify-content-center display-1 text-primary p-5'
+      className='d-flex justify-content-center display-1 text-danger p-5'
     />
   );
 };
