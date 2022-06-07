1e15a955f69d92d4e73531bdd9cd08124ab9fa02
shafa45 <safaullah14@gmail.com>
2022-06-07 12:28:24 +0530

    Become Instructor page completed

--- pages/user/become-instructor.jsx
+++ pages/user/become-instructor.jsx
@@ -1,11 +1,73 @@
 import axios from 'axios';
+import { useContext, useState } from 'react';
+import { Context } from '../../context';
+import { Button } from 'antd';
+import {
+  SettingOutlined,
+  UserSwitchOutlined,
+  LoadingOutlined,
+} from '@ant-design/icons';
+
+import { toast } from 'react-toastify';
+import UserRoute from '../../components/routes/userRoutes';
 
 const BecomeInstructor = () => {
+  // state
+  const [loading, setLoading] = useState(false);
+  const { state } = useContext(Context);
+  const { user } = state;
+
+  const handleBecomeInstructor = () => {
+    setLoading(true);
+    axios
+      .post('/api/make-instructor')
+      .then((res) => {
+        console.log(res.data);
+        //  window.location.href = res.data
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setLoading(false);
+        toast.error('Stripe On Boarding failed. Try Again!');
+      });
+  };
+
   return (
     <>
       <h1 className='jumbotron text-center square'>Become Instructor</h1>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6 offset-md-3 text-center'>
+            <div className='pt-4'>
+              <UserSwitchOutlined className='display-1 pb-3' />
+              <br />
+              <h2>Setup payout to publish courses on LearnX</h2>
+              <p className='lead text-warning'>
+                LearnX partners with stripe to transfer earning to your bank
+                account
+              </p>
+
+              <Button
+                className='mb-3 w-50'
+                type='primary'
+                // block
+                shape='round'
+                icon={loading ? <LoadingOutlined /> : <SettingOutlined />}
+                size='large'
+                onClick={handleBecomeInstructor}
+                disabled={user && user.role && user.role.includes('Instructor')}
+              >
+                {loading ? 'Processing...' : 'Payout Setup'}
+              </Button>
+              <div className='lead'>
+                You will be redirected to stripe to complete onboarding process.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
-
 export default BecomeInstructor;
