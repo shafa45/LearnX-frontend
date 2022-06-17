7a21a455a6e16197d0ce4445eae8b0059a9b9b6c
shafa45 <safaullah14@gmail.com>
2022-06-17 20:29:29 +0530

    Add lesson

--- pages/instructor/course/view/[slug].jsx
+++ pages/instructor/course/view/[slug].jsx
@@ -35,9 +35,24 @@ const CourseView = () => {
   };
 
   // Function for adding lessons
-  const handleAddLesson = (e) => {
+  const handleAddLesson = async (e) => {
     e.preventDefault();
-    console.log(values);
+    // console.log(values);
+    try {
+      const { data } = await axios.post(
+        `/api/course/lesson/${slug}/${course.instructor._id}`,
+        values
+      );
+      console.log(data);
+      setValues({ ...values, title: '', content: '', video: {} });
+      setVisible(false);
+      setUploadBtnText('Upload video');
+      setCourse(data);
+      toast.success('Lesson added');
+    } catch (err) {
+      console.log(err);
+      toast.error('Lesson addition failed.');
+    }
   };
 
   const handleVideo = async (e) => {
