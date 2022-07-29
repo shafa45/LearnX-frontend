ff0e00b693418b2b8c4f37187072da24278cfed4
shafa45 <safaullah14@gmail.com>
2022-07-29 22:59:21 +0530

    single course jumbotron

---
+++ components/cards/SingleCourseJumbotron.js
@@ -0,0 +1,84 @@
+import { currencyFormatter } from "../../utils/helpers";
+import { Badge, Modal } from 'antd';
+import ReactPlayer from 'react-player';
+
+
+
+const SingleCourseJumbotron = ({ course,
+showModal,
+setShowModal,
+preview,
+setPreview
+}) => {
+  const {
+    name,
+    description,
+    instructor,
+    updatedAt,
+    lessons,
+    image,
+    price,
+    paid,
+    category,
+  } = course;
+
+  return (
+    <div className='jumbotron bg-primary square'>
+      <div className='col-md-8'>
+        {/* {title} */}
+        <h1 className='text-light font-weight-bold'>{name}</h1>
+        {/* {description} */}
+        <p className='lead'>
+          {description && description.substring(0, 100)}...
+        </p>
+        {/* {category} */}
+        <Badge
+          count={category}
+          style={{ backgroundColor: '#87d068' }}
+          className='pb-2 mr-2'
+        />
+        {/* {author} */}
+        <p>Created by {instructor.name}</p>
+        {/* {updated at} */}
+        <p>Last updated {new Date(updatedAt).toLocaleDateString()}</p>
+        {/* {price} */}
+        <h4 className='text-light'>
+          {paid
+            ? currencyFormatter({ currency: 'USD', amount: price })
+            : 'Free'}
+        </h4>
+      </div>
+      <div className='col-md-4'>
+        {/* show video preview of course image */}
+
+        {lessons &&
+        lessons.length > 0 &&
+        lessons[0].video &&
+        lessons[0].video.Location ? (
+          <div
+            onClick={() => {
+              setPreview(lessons[0].video.Location);
+              setShowModal(!showModal);
+            }}
+          >
+        
+            <ReactPlayer
+              className='react-player-div'
+              url={lessons[0].video.Location}
+              width='100%'
+              light={image.Location}
+              height='275px'
+            //   controls={true}
+            />
+          </div>
+        ) : (
+          <img src={image.Location} alt={name} className='img-fluid' />
+        )}
+
+        {/* enroll button */}
+      </div>
+    </div>
+  );
+};
+
+export default SingleCourseJumbotron;
--- pages/course/[slug].js
+++ pages/course/[slug].js
@@ -1,87 +1,32 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import { Badge, Modal } from 'antd';
-import { currencyFormatter } from '../../utils/helpers';
-import ReactPlayer from 'react-player';
+
+import SingleCourseJumbotron from '../../components/cards/SingleCourseJumbotron';
 
 const SingleCourse = ({ course }) => {
-
-// state
-const [showModal, setShowModal] = useState(false);
-const [preview, setPreview]  = useState("")
+  // state
+  const [showModal, setShowModal] = useState(false);
+  const [preview, setPreview] = useState('');
 
   const router = useRouter();
   const { slug } = router.query;
 
-  const {
-    name,
-    description,
-    instructor,
-    updatedAt,
-    lessons,
-    image,
-    price,
-    paid,
-    category,
-  } = course;
 
   return (
     <>
       {/* <pre>{JSON.stringify(course,null,4)}</pre> */}
-      <div className='jumbotron bg-primary square'>
-        <div className='col-md-8'>
-          {/* {title} */}
-          <h1 className='text-light font-weight-bold'>{name}</h1>
-          {/* {description} */}
-          <p className='lead'>
-            {description && description.substring(0, 100)}...
-          </p>
-          {/* {category} */}
-          <Badge
-            count={category}
-            style={{ backgroundColor: '#87d068' }}
-            className='pb-2 mr-2'
-          />
-          {/* {author} */}
-          <p>Created by {instructor.name}</p>
-          {/* {updated at} */}
-          <p>Last updated  {new Date(updatedAt).toLocaleDateString()}</p>
-          {/* {price} */}
-          <h4 className='text-light'>
-            {paid
-              ? currencyFormatter({ currency: 'USD', amount: price })
-              : 'Free'}
-          </h4>
-        </div>
-        <div className='col-md-4'>
-          {/* show video preview of course image */}
 
-          {lessons && lessons.length > 0 && lessons[0].video &&lessons[0].video.Location ? (
-            <div onClick={() => {
-                setPreview(lessons[0].video.Location)
-                setShowModal(!showModal)
-            }}>
-              <ReactPlayer
-                className='react-player-div'
-                url={lessons[0].video.Location}
-                width='100%'
-                light={image.Location}
-                height='275px'
-                controls={true}
-              />
-            </div>
-          ) : (
-            <img src={image.Location} alt={name} className='img-fluid' />
-          )}
+      <SingleCourseJumbotron
+        course={course}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        preview={preview}
+        setPreview={setPreview}
 
-          {/* enroll button */}
-        </div>
-      </div>
+      />
 
-      {showModal ? 
-     
-      course.lessons[0].video.Location: "don'show" }
+      {showModal ? course.lessons[0].video.Location : "don'show"}
     </>
   );
 };
